d5e92eaafa504f115ac52823e35f791b241d48d8
Busireddy Raghu Nandan Reddy <b.raghunandan1998@gmail.com>
2018-05-17 22:48:02 +0530

    First commit

---
+++ topic_detection2.py
@@ -0,0 +1,52 @@
+text1 = open("poem.txt", "rb").read()
+
+
+
+from nltk.corpus import stopwords 
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+stop = set(stopwords.words('english'))
+exclude = set(string.punctuation) 
+lemma = WordNetLemmatizer()
+def clean(doc):
+    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+    return normalized
+
+doc_clean = [clean(text1).split()]
+
+# Importing Gensim
+import gensim
+from gensim import corpora
+
+
+# Creating the term dictionary of our courpus, where every unique term is assigned an index. 
+from six import iteritems
+ # collect statistics about all tokens
+dictionary = corpora.Dictionary(line.lower().split() for line in open('poem.txt'))
+ # remove stop words and words that appear only once
+stop_ids = [dictionary.token2id[stopword] for stopword in stop
+            if stopword in dictionary.token2id]
+once_ids = [tokenid for tokenid, docfreq in iteritems(dictionary.dfs) if docfreq == 1]
+dictionary.filter_tokens(stop_ids + once_ids)  # remove stop words and words that appear only once
+dictionary.compactify()  # remove gaps in id sequence after words that were removed
+print(dictionary)
+
+
+
+class MyCorpus(object):
+     def __iter__(self):
+         for line in open('poem.txt'):
+             # assume there's one document per line, tokens separated by whitespace
+             yield dictionary.doc2bow(line.lower().split())
+
+
+# Creating the object for LDA model using gensim library
+Lda = gensim.models.ldamodel.LdaModel
+
+# Running and Trainign LDA model on the document term matrix.
+ldamodel = Lda(MyCorpus(), num_topics=3, id2word = dictionary, passes=50)
+
+
+print(ldamodel.print_topics(num_topics=3, num_words=3))
